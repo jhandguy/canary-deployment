81759bb0e78534057fd684fbabb60185290b16a2
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2021-12-30 10:53:17 +0100

    feat: add prometheus metrics in sample-app

--- sample-app/main.go
+++ sample-app/main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
 )
+
+var (
+	requestCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "sample_app_requests_count",
+			Help: "Request counter",
+		},
+		[]string{"success"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(collectors.NewBuildInfoCollector())
+	prometheus.MustRegister(requestCounter)
+}
 
 type response struct {
 	Node      string `json:"node"`
@@ -14,14 +32,27 @@ type response struct {
 	Pod       string `json:"pod"`
 }
 
-func handleRequest(response response) func(http.ResponseWriter, *http.Request) {
+func handleSuccess(response response) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		requestCounter.
+			WithLabelValues("true").
+			Inc()
 		_ = json.NewEncoder(w).Encode(response)
 	}
 }
 
+func handleError(status int) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestCounter.
+			WithLabelValues("false").
+			Inc()
+		w.WriteHeader(status)
+	}
+}
+
 func main() {
-	port, _ := os.LookupEnv("HTTP_PORT")
+	metricsPort, prometheusEnabled := os.LookupEnv("METRICS_PORT")
+	httpPort, _ := os.LookupEnv("HTTP_PORT")
 	node, _ := os.LookupEnv("KUBERNETES_NODE")
 	namespace, _ := os.LookupEnv("KUBERNETES_NAMESPACE")
 	pod, _ := os.LookupEnv("KUBERNETES_POD")
@@ -30,6 +61,15 @@ func main() {
 		Namespace: namespace,
 		Pod:       pod,
 	}
-	http.HandleFunc("/", handleRequest(resp))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+	if prometheusEnabled {
+		metricsMux := http.NewServeMux()
+		metricsMux.Handle("/monitoring/metrics", promhttp.Handler())
+		go func() { log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", metricsPort), metricsMux)) }()
+	}
+
+	httpMux := http.NewServeMux()
+	httpMux.HandleFunc("/success", handleSuccess(resp))
+	httpMux.HandleFunc("/error", handleError(http.StatusInternalServerError))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", httpPort), httpMux))
 }
